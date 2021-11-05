358de2462a29e87109a749ce62a32dd59be51ab2
Superior <buenobrandron14@gmail.com>
2021-11-05 01:32:37 -0700

    Se agrega el proyecto

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+		<div class="wrapp">
+           
+            <p> Visitanos en nuestras<br>
+                <a href="https://www.bing.com/maps?q=sucursal+de+coppel+ciudad+juarez&go=Buscar&qs=ds&form=QBRE">SUCURSALES</a></p>
+            </div>
+	</footer>
